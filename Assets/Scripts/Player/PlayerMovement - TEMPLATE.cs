4830a22d5e456a1d1da8cd263f976d6e198b807d
Muhammad Zaki Amanullah <13521146@std.stei.itb.ac.id>
2024-04-28 13:23:28 +0700

    chore: scene for demo-ing gun shot

--- Assets/Scripts/Player/PlayerMovement - TEMPLATE.cs
+++ Assets/Scripts/Player/PlayerMovement - TEMPLATE.cs
@@ -5,122 +5,59 @@ namespace Nightmare
 {
     public class PlayerMovement : PausibleObject
     {
-        public float speed = 6f;            // The speed that the player will move at.
+        public float speed = 6f;
 
+        Vector3 movement;
+        Animator anim;
+        Rigidbody playerRigidBody;
+        int floorMask;
+        readonly float camRayLength = 100f;
 
-        Vector3 movement;                   // The vector to store the direction of the player's movement.
-        Animator anim;                      // Reference to the animator component.
-        Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-#if !MOBILE_INPUT
-        int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-        float camRayLength = 100f;          // The length of the ray from the camera into the scene.
-#endif
-
-        void Awake ()
+        private void Awake()
         {
-#if !MOBILE_INPUT
-            // Create a layer mask for the floor layer.
-            floorMask = LayerMask.GetMask ("Floor");
-#endif
-
-            // Set up references.
-            anim = GetComponent <Animator> ();
-            playerRigidbody = GetComponent <Rigidbody> ();
-
-            StartPausible();
+            floorMask = LayerMask.GetMask("Floor");
+            anim = GetComponent<Animator>();
+            playerRigidBody = GetComponent<Rigidbody>();
         }
 
-        void OnDestroy()
+        private void FixedUpdate()
         {
-            StopPausible();
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+
+            Move(h, v);
+            Turning();
+            Animating(h, v);
         }
 
-        void FixedUpdate ()
+        void Move(float h, float v)
         {
-            if (isPaused)
-                return;
+            movement.Set(h, 0f, v);
 
-            // Store the input axes.
-            float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
-            float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
-
-            // Move the player around the scene.
-            Move (h, v);
-
-            // Turn the player to face the mouse cursor.
-            Turning ();
-
-            // Animate the player.
-            Animating (h, v);
-        }
-
-
-        void Move (float h, float v)
-        {
-            // Set the movement vector based on the axis input.
-            movement.Set (h, 0f, v);
-            
-            // Normalise the movement vector and make it proportional to the speed per second.
             movement = movement.normalized * speed * Time.deltaTime;
 
-            // Move the player to it's current position plus the movement.
-            playerRigidbody.MovePosition (transform.position + movement);
+            playerRigidBody.MovePosition(transform.position + movement);
         }
 
-
-        void Turning ()
+        void Turning()
         {
-#if !MOBILE_INPUT
-            // Create a ray from the mouse cursor on screen in the direction of the camera.
-            Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            // Create a RaycastHit variable to store information about what was hit by the ray.
-            RaycastHit floorHit;
-
-            // Perform the raycast and if it hits something on the floor layer...
-            if(Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+            if (Physics.Raycast(camRay, out RaycastHit floorHit, camRayLength, floorMask))
             {
-                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
                 Vector3 playerToMouse = floorHit.point - transform.position;
-
-                // Ensure the vector is entirely along the floor plane.
                 playerToMouse.y = 0f;
 
-                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-                Quaternion newRotatation = Quaternion.LookRotation (playerToMouse);
-
-                // Set the player's rotation to this new rotation.
-                playerRigidbody.MoveRotation (newRotatation);
+                Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+                playerRigidBody.MoveRotation(newRotation);
             }
-#else
-
-            Vector3 turnDir = new Vector3(CrossPlatformInputManager.GetAxisRaw("Mouse X") , 0f , CrossPlatformInputManager.GetAxisRaw("Mouse Y"));
-
-            if (turnDir != Vector3.zero)
-            {
-                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
-                Vector3 playerToMouse = (transform.position + turnDir) - transform.position;
-
-                // Ensure the vector is entirely along the floor plane.
-                playerToMouse.y = 0f;
-
-                // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-                Quaternion newRotatation = Quaternion.LookRotation(playerToMouse);
-
-                // Set the player's rotation to this new rotation.
-                playerRigidbody.MoveRotation(newRotatation);
-            }
-#endif
         }
 
-
-        void Animating (float h, float v)
+        void Animating(float h, float v)
         {
-            // Create a boolean that is true if either of the input axes is non-zero.
             bool walking = h != 0f || v != 0f;
-
-            // Tell the animator whether or not the player is walking.
-            anim.SetBool ("IsWalking", walking);
+            anim.SetBool("IsWalking", walking);
         }
+
     }
 }
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -10,148 +10,160 @@ namespace Nightmare
         public int damagePerShot = 20;
         public float timeBetweenBullets = 0.15f;
         public float range = 100f;
-        public GameObject grenade;
-        public float grenadeSpeed = 200f;
-        public float grenadeFireDelay = 0.5f;
+        //public GameObject grenade;
+        //public float grenadeSpeed = 200f;
+        //public float grenadeFireDelay = 0.5f;
 
         float timer;
-        Ray shootRay = new Ray();
+        Ray shootRay;
         RaycastHit shootHit;
         int shootableMask;
         ParticleSystem gunParticles;
         LineRenderer gunLine;
         AudioSource gunAudio;
         Light gunLight;
-		public Light faceLight;
+        //public Light faceLight;
         float effectsDisplayTime = 0.2f;
-        int grenadeStock = 99;
-  
-        private UnityAction listener;
+        //int grenadeStock = 99;
 
-        void Awake ()
+        //private UnityAction listener;
+
+        void Awake()
         {
             // Create a layer mask for the Shootable layer.
-            shootableMask = LayerMask.GetMask ("Shootable", "Enemy");
+            shootableMask = LayerMask.GetMask("Shootable"/*, "Enemy"*/);
 
             // Set up the references.
-            gunParticles = GetComponent<ParticleSystem> ();
-            gunLine = GetComponent <LineRenderer> ();
-            gunAudio = GetComponent<AudioSource> ();
-            gunLight = GetComponent<Light> ();
-			//faceLight = GetComponentInChildren<Light> ();
+            gunParticles = GetComponent<ParticleSystem>();
+            gunLine = GetComponent<LineRenderer>();
+            gunAudio = GetComponent<AudioSource>();
+            gunLight = GetComponent<Light>();
+            //faceLight = GetComponentInChildren<Light> ();
 
-            AdjustGrenadeStock(0);
+            //AdjustGrenadeStock(0);
 
-            listener = new UnityAction(CollectGrenade);
+            //listener = new UnityAction(CollectGrenade);
 
-            EventManager.StartListening("GrenadePickup", CollectGrenade);
+            //EventManager.StartListening("GrenadePickup", CollectGrenade);
 
-            StartPausible();
+            //StartPausible();
         }
 
-        void OnDestroy()
-        {
-            EventManager.StopListening("GrenadePickup", CollectGrenade);
-            StopPausible();
-        }
+        //void OnDestroy()
+        //{
+        //    EventManager.StopListening("GrenadePickup", CollectGrenade);
+        //    StopPausible();
+        //}
 
-        void Update ()
+        void Update()
         {
-            if (isPaused)
-                return;
+            //            if (isPaused)
+            //                return;
 
-            // Add the time since Update was last called to the timer.
+            //            // Add the time since Update was last called to the timer.
+            //            timer += Time.deltaTime;
+
+            //#if !MOBILE_INPUT
+            //            if (timer >= timeBetweenBullets && Time.timeScale != 0)
+            //            {
+            //                // If the Fire1 button is being press and it's time to fire...
+            //                if (Input.GetButton("Fire2") && grenadeStock > 0)
+            //                {
+            //                    // ... shoot a grenade.
+            //                    ShootGrenade();
+            //                }
+
+            //                // If the Fire1 button is being press and it's time to fire...
+            //                else if (Input.GetButton("Fire1"))
+            //                {
+            //                    // ... shoot the gun.
+            //                    Shoot();
+            //                }
+            //            }
+
+            //#else
+            //            // If there is input on the shoot direction stick and it's time to fire...
+            //            if ((CrossPlatformInputManager.GetAxisRaw("Mouse X") != 0 || CrossPlatformInputManager.GetAxisRaw("Mouse Y") != 0) && timer >= timeBetweenBullets)
+            //            {
+            //                // ... shoot the gun
+            //                Shoot();
+            //            }
+            //#endif
+            //            // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
+            //            if(timer >= timeBetweenBullets * effectsDisplayTime)
+            //            {
+            //                // ... disable the effects.
+            //                DisableEffects ();
+            //            }
+
             timer += Time.deltaTime;
 
-#if !MOBILE_INPUT
-            if (timer >= timeBetweenBullets && Time.timeScale != 0)
+            if (Input.GetButton("Fire1") && timer >= timeBetweenBullets)
             {
-                // If the Fire1 button is being press and it's time to fire...
-                if (Input.GetButton("Fire2") && grenadeStock > 0)
-                {
-                    // ... shoot a grenade.
-                    ShootGrenade();
-                }
-
-                // If the Fire1 button is being press and it's time to fire...
-                else if (Input.GetButton("Fire1"))
-                {
-                    // ... shoot the gun.
-                    Shoot();
-                }
-            }
-            
-#else
-            // If there is input on the shoot direction stick and it's time to fire...
-            if ((CrossPlatformInputManager.GetAxisRaw("Mouse X") != 0 || CrossPlatformInputManager.GetAxisRaw("Mouse Y") != 0) && timer >= timeBetweenBullets)
-            {
-                // ... shoot the gun
                 Shoot();
             }
-#endif
-            // If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
-            if(timer >= timeBetweenBullets * effectsDisplayTime)
+
+            if (timer >= timeBetweenBullets * effectsDisplayTime)
             {
-                // ... disable the effects.
-                DisableEffects ();
+                DisableEffects();
             }
         }
 
 
-        public void DisableEffects ()
+        public void DisableEffects()
         {
             // Disable the line renderer and the light.
             gunLine.enabled = false;
-			faceLight.enabled = false;
+            //faceLight.enabled = false;
             gunLight.enabled = false;
         }
 
 
-        void Shoot ()
+        void Shoot()
         {
             // Reset the timer.
             timer = 0f;
 
             // Play the gun shot audioclip.
-            gunAudio.Play ();
+            gunAudio.Play();
 
             // Enable the lights.
             gunLight.enabled = true;
-			faceLight.enabled = true;
+            //faceLight.enabled = true;
 
             // Stop the particles from playing if they were, then start the particles.
-            gunParticles.Stop ();
-            gunParticles.Play ();
+            gunParticles.Stop();
+            gunParticles.Play();
 
             // Enable the line renderer and set it's first position to be the end of the gun.
             gunLine.enabled = true;
-            gunLine.SetPosition (0, transform.position);
+            gunLine.SetPosition(0, transform.position);
 
             // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
             shootRay.origin = transform.position;
             shootRay.direction = transform.forward;
 
             // Perform the raycast against gameobjects on the shootable layer and if it hits something...
-            if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+            if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
             {
                 // Try and find an EnemyHealth script on the gameobject hit.
-                EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
 
                 // If the EnemyHealth component exist...
-                if(enemyHealth != null)
+                if (enemyHealth != null)
                 {
                     // ... the enemy should take damage.
-                    enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+                    enemyHealth.TakeDamage(damagePerShot, shootHit.point);
                 }
 
                 // Set the second position of the line renderer to the point the raycast hit.
-                gunLine.SetPosition (1, shootHit.point);
+                gunLine.SetPosition(1, shootHit.point);
             }
             // If the raycast didn't hit anything on the shootable layer...
             else
             {
                 // ... set the second position of the line renderer to the fullest extent of the gun's range.
-                gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+                gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
             }
         }
 
@@ -166,26 +178,26 @@ namespace Nightmare
             gunLine.widthCurve = curve;
         }
 
-        public void CollectGrenade()
-        {
-            AdjustGrenadeStock(1);
-        }
+        //public void CollectGrenade()
+        //{
+        //    AdjustGrenadeStock(1);
+        //}
 
-        private void AdjustGrenadeStock(int change)
-        {
-            grenadeStock += change;
-            GrenadeManager.grenades = grenadeStock;
-        }
+        //private void AdjustGrenadeStock(int change)
+        //{
+        //    grenadeStock += change;
+        //    GrenadeManager.grenades = grenadeStock;
+        //}
 
-        void ShootGrenade()
-        {
-            AdjustGrenadeStock(-1);
-            timer = timeBetweenBullets - grenadeFireDelay;
-            GameObject clone = PoolManager.Pull("Grenade", transform.position, Quaternion.identity);
-            EventManager.TriggerEvent("ShootGrenade", grenadeSpeed * transform.forward);
-            //GameObject clone = Instantiate(grenade, transform.position, Quaternion.identity);
-            //Grenade grenadeClone = clone.GetComponent<Grenade>();
-            //grenadeClone.Shoot(grenadeSpeed * transform.forward);
-        }
+        //void ShootGrenade()
+        //{
+        //    AdjustGrenadeStock(-1);
+        //    timer = timeBetweenBullets - grenadeFireDelay;
+        //    GameObject clone = PoolManager.Pull("Grenade", transform.position, Quaternion.identity);
+        //    EventManager.TriggerEvent("ShootGrenade", grenadeSpeed * transform.forward);
+        //    //GameObject clone = Instantiate(grenade, transform.position, Quaternion.identity);
+        //    //Grenade grenadeClone = clone.GetComponent<Grenade>();
+        //    //grenadeClone.Shoot(grenadeSpeed * transform.forward);
+        //}
     }
 }
