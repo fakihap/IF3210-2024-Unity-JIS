3e8ce26e6c230e8ab707d6926ebcb7ba14b12bf1
Rifqi222 <mrifki193@gmail.com>
2024-05-09 16:31:26 +0700

    feat: add shop closed building

--- Assets/Scripts/Managers/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     public GameObject shopBuilding;
+    public GameObject shopBuildingClosed;
     private Animator _anim;
     private ShopBuildingEffect shopBuildingEffect;
+    private ShopBuildingClosedEffect shopBuildingClosedEffect;
     public GameObject shopCanvas;
     // public GameObject enemyManager;
     private float startTime;
@@ -20,62 +22,86 @@ public class ShopManager : MonoBehaviour
     {
         _anim = GetComponent<Animator>();
         shopBuildingEffect = shopBuilding.GetComponent<ShopBuildingEffect>();
+        shopBuildingClosedEffect = shopBuildingClosed.GetComponent<ShopBuildingClosedEffect>();
         shopCanvas.SetActive(false);
         startTime = Time.time;
         PauseManager.StaticPauseOrUnPause();
         shopBuilding.SetActive(true);
+        shopBuildingClosed.SetActive(false);
         // enemyManager.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    // Update is called once per frame
+void Update()
+{
+    // Check if the shop has closed
+    if ((Time.time - startTime) > (timeLimit + 5))
     {
-        if (!shopBuilding.activeSelf)
-        {
-            return;
-        }
+        shopBuilding.SetActive(false);
+        shopBuildingClosed.SetActive(true);
+    }
 
-        if ((Time.time - startTime) > (timeLimit + 5))
-        {
-            shopBuilding.SetActive(false);
-            // enemyManager.SetActive(true);
-            
-            return;
-        }
+    // Update shop effects components
+    shopBuildingEffect = shopBuilding.GetComponent<ShopBuildingEffect>();
+    shopBuildingClosedEffect = shopBuildingClosed.GetComponent<ShopBuildingClosedEffect>();
 
-        if (Time.time - startTime > timeLimit)
-        {
-            shopBuildingEffect.isPlayerInShopRange = true;
-            return;
-        }
+    // Debug logs to check player range detection
+    Debug.Log("Is Player in open shop range: " + shopBuildingEffect.isPlayerInShopRange);
+    Debug.Log("Is Player in closed shop range: " + shopBuildingClosedEffect.isPlayerInShopRange);
 
+    // Check if the open shop is active
+    if (shopBuilding.activeSelf)
+    {
         if (shopBuildingEffect.isPlayerInShopRange)
         {
-            Debug.Log("Masuk Range");
+            // Player is in the range of the open shop
+            _anim.SetBool("ShopClosed", false);
             _anim.SetBool("IsPlayerInRange", true);
         }
         else
         {
             _anim.SetBool("IsPlayerInRange", false);
         }
-
-        if (Input.GetKey(KeyCode.B) && !PauseManager.IsPaused())
+    }
+    // Check if the closed shop is active
+    else if (shopBuildingClosed.activeSelf)
+    {
+        if (shopBuildingClosedEffect != null && shopBuildingClosedEffect.isPlayerInShopRange)
         {
-            PauseManager.IsPaused();
-            Debug.Log("Press B");
-            if (!shopBuildingEffect.isPlayerInShopRange)
-            {
-                Debug.Log("False");
-                _anim.SetTrigger(BuyErrorClick);
-            }
-            else
-            {
-                // PauseManager.StaticPauseOrUnPause();
-                PauseManager.Pause();
-                // PauseManager.healthCanvas.enabled = false;
-                // PauseManager.pauseCanvas.enabled = false;                    
-                shopCanvas.SetActive(true);
-            }
+            // Player is in the range of the closed shop
+            _anim.SetBool("ShopClosed", true);
+            _anim.SetBool("IsPlayerInRange", true);
+        }
+        else
+        {
+            _anim.SetBool("IsPlayerInRange", false);
         }
     }
+
+    // Handle player input
+    if (Input.GetKey(KeyCode.B) && !PauseManager.IsPaused())
+    {
+        PauseManager.IsPaused();
+        Debug.Log("Press B");
+        if (!shopBuildingEffect.isPlayerInShopRange && shopBuilding.activeSelf)
+        {
+            // Player is not in the range of the open shop
+            Debug.Log("False");
+            _anim.SetTrigger(BuyErrorClick);
+        }
+        else if(shopBuildingClosed.activeSelf)
+        {
+            _anim.SetBool("ShopClosed", true);
+            _anim.SetBool("IsPlayerInRange", true);
+        }
+        else
+        {
+            // Player is in the range of the open shop
+            PauseManager.Pause();
+            shopCanvas.SetActive(true);
+        }
+    }
+}
+
 }
